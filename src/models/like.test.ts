79e68afbb7d672f8fc8c16faea161def61f37745
Rerrah <rerrah57@gmail.com>
2026-05-17 14:04:47 +0900

    Fix errors in test

--- src/models/like.test.ts
+++ src/models/like.test.ts
@@ -3,13 +3,19 @@
 
 import { describe, expect, it } from "vitest";
 import { toggleLike } from "./like";
+import { createUserId } from "./user";
 
 describe("toggleLike", () => {
   it("should work correctly", () => {
-    expect(toggleLike(["hoge"], "fuga")).toEqual(["hoge", "fuga"]);
-    expect(toggleLike(["hoge", "fuga", "piyo"], "fuga")).toEqual([
-      "hoge",
-      "piyo",
+    expect(toggleLike([createUserId("hoge")], createUserId("fuga"))).toEqual([
+      createUserId("hoge"),
+      createUserId("fuga"),
     ]);
+    expect(
+      toggleLike(
+        [createUserId("hoge"), createUserId("fuga"), createUserId("piyo")],
+        createUserId("fuga")
+      )
+    ).toEqual([createUserId("hoge"), createUserId("piyo")]);
   });
 });
